d42bab5fab14a88c3ea16570b50abd1cc8b83af1
estagiario <estagiario@SONIC.ICAD>
2025-10-10 20:17:05 -0300

    Adiciona script MovimentoPlayer

---
+++ 2025_2-time_2/Assets/Scripts/MovimentoPlayer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoPlayer : MonoBehaviour
+{
+    public float MoveSpeed = 5.0f;
+
+    private Rigidbody2D rb;
+
+    private float MoveInput;
+
+    public float JumpForce = 5.0f;
+
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Move()
+    {
+        MoveInput = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(MoveInput * MoveSpeed, rb.velocity.y);
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.velocity = new Vector2(0, JumpForce);
+
+    }
+    private void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+
+
+
+
+}

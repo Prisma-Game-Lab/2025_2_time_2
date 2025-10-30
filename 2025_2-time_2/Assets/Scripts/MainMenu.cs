26cb80246418aaa2f4067129b80785d69bca78df
Jesmius <lucas.falvaren@gmail.com>
2025-10-30 15:33:56 -0300

    Added Main Menu Music + Level 1 Music

--- 2025_2-time_2/Assets/Scripts/MainMenu.cs
+++ 2025_2-time_2/Assets/Scripts/MainMenu.cs
@@ -6,8 +6,19 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
+    [SerializeField] private string menuMusic;
+    [SerializeField] private string levelMusic;
+    void Start()
+    {
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.PlayMusic(menuMusic);
+        }
+    }
+
    public void Play(string sceneName)
     {
+        AudioManager.Instance.PlayMusic(levelMusic);
         SceneManager.LoadSceneAsync(sceneName);
     }
    public void Quit()
